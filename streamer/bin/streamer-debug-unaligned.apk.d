cab9d3045f9f333749ef6c871d470d11ea933667
Steffen Gauglitz <sgauglitz@AlloProto01.(none)>
2013-05-15 15:39:32 -0700

    Streamer code from "ohbee", ohbee / android / streamer
    built according to instructions on:
     http://stackoverflow.com/questions/10821015/anybody-has-successfully-ported-live555-to-android

---
+++ streamer/bin/streamer-debug-unaligned.apk.d
@@ -0,0 +1,7 @@
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/bin/streamer-debug-unaligned.apk : \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/bin/streamer.ap_ \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/bin/classes.dex \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/sdk/tools/support/annotations.jar \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/libs/jnix.jar \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/libs/armeabi/libstreamer.so \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/libs/armeabi/libjnix.so \
---
+++ streamer/gen/R.java.d
@@ -0,0 +1,5 @@
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/gen/ob/android/R.java \
+ : /home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/res/drawable/icon.png \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/res/values/strings.xml \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/bin/res/drawable/icon.png \
+/home/sgauglitz/adt-bundle-linux-x86_64-20130219/ohbee/android/streamer/bin/AndroidManifest.xml \
